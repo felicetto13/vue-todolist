909434995647476dbabcb67a438adf5f405cc474
felicetto13 <felice.laterza93@icloud.com>
2022-06-14 01:18:39 +0200

    first bonus point done

---
+++ bonus/js/app.js
@@ -0,0 +1,59 @@
+
+const app = new Vue(
+    {
+        el: '#app',
+        data:
+        {
+            toDoList:[
+                {
+                text : "fare la spesa",
+                done: false
+            },
+            {
+                text : "fare shopping",
+                done: false
+            },
+            {
+                text : "andare al mare",
+                done: false
+            },
+            {
+                text : "chiamare idraulico",
+                done: false
+            },
+            ],
+            editMode: false,
+            newTasks: "",
+        },
+        methods:
+        {
+            openEditMode(){
+                if(this.editMode){
+                    this.editMode = false
+                }
+                else if(!this.editMode){
+                    this.editMode = true
+                }
+            },
+            taskDone(index){
+                if(!this.toDoList[index].done){
+                    this.toDoList[index].done = true;
+                }
+                else if(this.toDoList[index].done){
+                    this.toDoList[index].done = false;
+                }
+            },
+            removeTasks(index){
+                this.toDoList.splice(index, 1);
+            },
+            addTasks(){
+
+                this.toDoList.push({text: this.newTasks.trim(), done: false})
+                
+                this.newTasks = "";
+                this.editMode = false
+
+            }
+        }
+    }
+);
